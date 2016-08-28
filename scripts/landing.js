ff2e656c7949ccfb95905d3baf7cc452a9b97b97
Mallory Robertson <mallory@mal.works>
2016-08-27 17:46:12 -0700

    Checkpoint 9: DOM Scripting: Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,13 +1,28 @@
 
-var revealPoint = function() {
-    var point = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-    for (var i = 0; i < point.length; i++) {
-        point[i].style.opacity = 1;
-        point[i].style.transform = "scaleX(1) translateY(0)";
-        point[i].style.msTransform = "scaleX(1) translateY(0)";
-        point[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(point) {
+    forEach(point, revealPoint);
+};
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        revealPoint(pointsArray);
+    }  
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 400;
+
+    window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop.getBoundingClientRect().top + " pixels"); {
+        animatePoints(pointsArray);
+        }
+    });
 }
-
-revealPoint();
