7d79b14188671e52c292e0f5a4ea9355dc8fc44c
Mallory Robertson <mallory@mal.works>
2016-08-30 18:55:00 -0700

    Fixing tbody

--- scripts/album.js
+++ scripts/album.js
@@ -37,12 +37,13 @@ var albumMarconi = {
 	songs: [
 		{ title: "Hello, Operator?", duration: "1:01" },
         { title: "Ring, ring, ring", duration: "5:01" },
-        { title: "Fits in your pocket", duration: "3:21"},
+        { title: "Fits in your pocket", duration: "3:21" },
         { title: "Can you hear me now?", duration: "3:14" },
         { title: "Wrong phone number", duration: "2:15"}
      ]
 };
 
+// this makes one table row for the album's songs
 var createSongRow = function(songNumber, songName, songLength) {
 	var template =
 		'<tr class="album-view-song-item">'
@@ -54,38 +55,41 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 	return template;
 
-};
+}; // this creates a lot of <tbody> tags, why is that and is that okay?
 
+// this block gives js variables to the css classes
+// why wouldn't we just hardcode this like we did in the album details?
 var albumTitle = document.getElementsByClassName('album-view-title')[0];
 var albumArtist = document.getElementsByClassName('album-view-artist')[0];
 var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
 var albumImage = document.getElementsByClassName('album-cover-art')[0];
-var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0].childNodes[1];
+
 
 var setCurrentAlbum = function(album) {
-
+	//a little confused about how these assignments work without assigning them variables
 	albumTitle.firstChild.nodeValue = album.title;
 	albumArtist.firstChild.nodeValue = album.artist;
 	albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-	albumImage.setAttribute('src', album.albumArtUrl);
+	albumImage.setAttribute('src', album.albumArtUrl); // what?
 
 	albumSongList.innerHTML = ' ';
-
+	// this loop populates the album song list with the information in the album object
 	for (var i = 0; i < album.songs.length; i++) {
 		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
 	}
 };
-
+// when the window is loaded, do this:
 window.onload = function() {
-	setCurrentAlbum(albumPicasso);
+	setCurrentAlbum(albumPicasso); // 1. load album picasso first.
 
 	var albums = [albumPicasso, albumMarconi, albumDuchamp];
 	var index = 1;
-	albumImage.addEventListener("click", function(event) {
-		setCurrentAlbum(albums[index]);
-		index++;
-		if (index == albums.length) {
-			index = 0;
+	albumImage.addEventListener("click", function(event) { // on the event of click on the image, do this:
+		setCurrentAlbum(albums[index]); // run setCurrentAlbum to the index (this loads at 0 but first click populates 1)
+		index++; // now add one to index, so number two will load on next click
+		if (index == albums.length) { // if we reach the last one
+			index = 0; // load the first album again
 		}
 	});
 };
