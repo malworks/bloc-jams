21f6d434c7b28e3c206c22982be0e1d94291d09b
Mallory Robertson <mallory@mal.works>
2016-09-09 16:23:06 -0700

    Checkpoint 18: jQuery Play/Pause

--- scripts/album.js
+++ scripts/album.js
@@ -53,23 +53,61 @@ var createSongRow = function(songNumber, songName, songLength) {
 	+ 	'</tr>'
 	;
 
-	return $(template);
+	var $row =  $(template);
 
-}; // this creates a lot of <tbody> tags, why is that and is that okay?
+	var clickHandler = function() {
+		var songNumber = $(this).attr('data-song-number');
+
+		if (currentlyPlayingSong !== null) {
+
+			var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
+			currentlyPlayingCell.html(currentlyPlayingSong);
+		}
+
+		if (currentlyPlayingSong !== songNumber) {
+			$(this).html(pauseButtonTemplate);
+			currentlyPlayingSong = songNumber;
+		} else if (currentlyPlayingSong === songNumber) {
+			$(this).html(pauseButtonTemplate);
+			currentlyPlayingSong = null;
+		}
+
+	};
+
+	var onHover = function(event) {
+	var songNumberCell = $(this).find('.song-item-number')
+	var songNumber = songNumberCell.attr('data-song-number');
+
+	if (songNumber !== currentlyPlayingSong) {
+		songNumberCell.html(playButtonTemplate);
+	}
+};
+
+	var offHover = function(event) {
+		var songNumberCell = $(this).find('.song-item-number');
+		var songNumber = songNumberCell.attr('data-song-number');
+
+		if (songNumber !== currentlyPlayingSong) {
+			songNumberCell.html(songNumber);
+		}
+	};
+
+	$row.find('.song-item-number').click(clickHandler);
+	$row.hover(onHover, offHover);
+	return $row;
+
+};
 
 var $albumTitle = $('album-view-title');
 var $albumArtist = $('album-view-artist');
 var $albumReleaseInfo = $('album-view-release-info');
 var $albumImage = $('.album-cover-art');
 var $albumSongList = $('.album-view-song-list');
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
 var setCurrentAlbum = function(album) {
-	//a little confused about how these assignments work without assigning them variables
      $albumTitle.text(album.title);
      $albumArtist.text(album.artist);
      $albumReleaseInfo.text(album.year + ' ' + album.label);
@@ -87,85 +125,8 @@ var setCurrentAlbum = function(album) {
 var currentlyPlayingSong = null;
 
 // when the window is loaded, do this:
-window.onload = function() {
+$(document).ready(function() {
 	setCurrentAlbum(albumPicasso); // 1. load album picasso first.
-
-	var findParentByClassName = function(element, targetClass) {
-		if (element) {
-			var currentParent = element.parentElement;
-			while (currentParent.className != targetClass && currentParent.className !== null) {
-				currentParent = currentParent.parentElement;
-			} if (currentParent === undefined) {
-				alert("No parent found."); // if a parent doesn't exist, return this alert
-
-			} if (currentParent.className != targetClass) {
-				alert("No parent found with that class name."); // if no parent exists with that class name
-			}
-			return currentParent;
-		}
-	}
-
-	var getSongItem = function(element) {
-		switch(element.className) {
-			case 'ion-play':
-			case 'ion-pause':
-			case 'album-song-button':
-				return findParentByClassName(element, 'song-item-number');
-			case 'album-view-song-item': 
-				return element.querySelector('.song-item-number');
-			case 'song-item-title':
-			case 'song-item-duration':
-				return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-			case 'song-item-number':
-				return element;
-			default:
-				return;
-		}
-	};
-
-	var clickHandler = function(targetElement) {
-		var songItem = getSongItem(targetElement);
-
-		if (currentlyPlayingSong === null) {
-			songItem.innerHTML = pauseButtonTemplate;
-			currentlyPlayingSong = songItem.getAttribute('data-song-number');
-		} else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-			songItem.innerHTML = playButtonTemplate;
-			currentlyPlayingSong = null;
-		} else if (currentlyPlayingSong != songItem.getAttribute('data-song-number')) {
-			var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-			currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-			songItem.innerHTML = pauseButtonTemplate;
-			currentlyPlayingSong = songItem.getAttribute('data-song-number');
-		}
-	};
-
-
-	songListContainer.addEventListener("mouseover", function(event) {
-	    if (event.target.parentElement.className === 'album-view-song-item') {
-        	var songItem = getSongItem(event.target);
-
-        	if (songItem.getAttribute('data-song-number') != currentlyPlayingSong) {
-        		songItem.innerHTML = playButtonTemplate;
-        	}
-        }
-
-        for (var i = 0; i < songRows.length; i++) {
-         songRows[i].addEventListener('mouseleave', function(event) {
-        	var songItem = getSongItem(event.target);
-        	var songItemNumber = songItem.getAttribute('data-song-number');
-
-        	if(songItemNumber != currentlyPlayingSong) {
-        		songItem.innerHTML = songItemNumber;
-        	}
-         });
-
-         songRows[i].addEventListener('click', function(event) {
-         	clickHandler(event.target);
-         });
-     }
-		
-	});
 
 	var albums = [albumPicasso, albumMarconi, albumDuchamp];
 	var index = 1;
@@ -176,6 +137,6 @@ window.onload = function() {
 			index = 0; // load the first album again
 		}
 	});
-};
+});
 
 
